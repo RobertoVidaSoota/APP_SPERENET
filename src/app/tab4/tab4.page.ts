93d0e00e6434eeb52ada35b94eef86d8989c19c8
RobertoVida <roberto50005000@gmail.com>
2022-02-03 17:17:30 -0300

    MIGRAR CÓDIGO LOGIN (ok) -- qui,  3 de fev de 2022 17:17:30

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
+  @ViewChild(IonContent) content: IonContent
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // FUNÇÃO PARA COLOCAR ROLAGEM AUTOMÁTICA PARA O TOPO
+  ionViewDidEnter()
+  {
+    setTimeout(()=>{
+      var tab = document.querySelector("ion-tab-button[aria-selected=true]")
+      let myTab = localStorage.getItem("tab")
+      var refContent = this.content
+
+      tab.addEventListener("click", function(){
+        if(myTab == "tab4")
+        {
+          refContent.scrollToTop(200)
+        }
+      });
+      
+    }, 80)
   }
 
 }
