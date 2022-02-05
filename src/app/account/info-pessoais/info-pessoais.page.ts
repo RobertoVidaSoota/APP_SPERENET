09881e209f170667046907128f6a2a8f046ad746
RobertoVida <roberto50005000@gmail.com>
2022-02-04 21:36:59 -0300

    MIGRAR CÓDIGO RECUPERAR INFO PESSOAIS (ok) -- sex,  4 de fev de 2022 21:36:58

--- src/app/account/info-pessoais/info-pessoais.page.ts
+++ src/app/account/info-pessoais/info-pessoais.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info-pessoais',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoPessoaisPage implements OnInit {
 
+  // @Input() myfocus:string
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter()
+  {
+    let input = document.getElementById("my_auto_focus")
+
+    input.focus()
   }
 
 }
