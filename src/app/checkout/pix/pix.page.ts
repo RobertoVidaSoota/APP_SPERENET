aa91e4c94a80600f91e5bbf033a8421c3405232d
RobertoVida <roberto50005000@gmail.com>
2022-02-09 22:05:03 -0300

    MIGRAR CÓDIGO PIX (ok) -- qua,  9 de fev de 2022 22:05:02

--- src/app/checkout/pix/pix.page.ts
+++ src/app/checkout/pix/pix.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pix',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PixPage implements OnInit {
 
-  constructor() { }
+  isOpen:boolean = false;
 
-  ngOnInit() {
+  constructor(
+  ) { }
+
+  ngOnInit(){
   }
+
+  myPopover()
+  {
+    this.isOpen = !this.isOpen;
+  }
+
 
 }
