8995fe700cd11007bdaa6a7780fd50b07672dbee
RobertoVida <roberto50005000@gmail.com>
2022-02-13 23:49:23 -0300

    MIGRAR CÓDIGO RASTRAR, mapa ta bugado (ok) -- dom, 13 de fev de 2022 23:49:23

--- src/app/shop/rastrear/rastrear.page.ts
+++ src/app/shop/rastrear/rastrear.page.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
+import * as L from 'leaflet'
+
+import 'leaflet-routing-machine';
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+
+
 @Component({
   selector: 'app-rastrear',
   templateUrl: './rastrear.page.html',
@@ -8,13 +15,42 @@ import { NavController } from '@ionic/angular';
 })
 export class RastrearPage implements OnInit {
 
+  map: L.map
+
   constructor(
     private navCtrl: NavController,
   )
   {}
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    this.map = L.map("map").setView([-12.879736, -38.312165], 14)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    {
+      // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    let myIcon = L.icon({
+      iconUrl: "../../../assets/markericon2x.png",
+      iconSize: [38, 45]
+    })
+
+    L.marker([-12.879736, -38.312165], {icon: myIcon}).addTo(this.map);
+
+    L.Routing.control({
+      waypoints: [
+        L.latLng(-12.879736, -38.312165),
+        L.latLng(-12.939894, -38.502955)
+      ],
+      routeWhileDragging: true
+    }).addTo(this.map);
+
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 0)
   }
+
 
   // FUNÇÃO PARA COLOCAR EM TODAS AS PÁGINAS SECUNDÁRIAS DAS ABAS
   ionViewDidEnter()
